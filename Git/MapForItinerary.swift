4c6570385546fef8bda148ba5cf4565ca73c96f0
Otomares Dantas <oto@otomaresmacbook.home>
2017-11-27 13:39:42 -0200

    Criei MapForItinerary.swift

---
+++ Git/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  Git
+//
+//  Created by Otomares Dantas on 27/11/17.
+//  Copyright © 2017 San Blas Studio. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
